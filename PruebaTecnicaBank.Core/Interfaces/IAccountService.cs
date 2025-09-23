93a8caf5a420b8cda194f014e4fb5ee33aec2235
gaitan19 <kenleyjos619@gmail.com>
2025-09-23 16:18:27 -0600

    Definir interfaces de servicios y Configurar AutoMapper con perfiles para Client, Account y Transaction

---
+++ PruebaTecnicaBank.Core/Interfaces/IAccountService.cs
@@ -0,0 +1,52 @@
+﻿using PruebaTecnicaBank.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaBank.Core.Interfaces
+{
+    /// <summary>
+    /// Interfaz que define los servicios relacionados con las cuentas bancarias.
+    /// </summary>
+    public interface IAccountService
+    {
+        /// <summary>
+        /// Crea una nueva cuenta bancaria.
+        /// </summary>
+        /// <param name="accountDto">Objeto que contiene los datos necesarios para crear la cuenta.</param>
+        /// <returns>Un objeto que representa la respuesta de la creación de la cuenta.</returns>
+        Task<AccountResponseDto> CreateAccountAsync(AccountCreateDto accountDto);
+
+        /// <summary>
+        /// Obtiene el saldo de una cuenta bancaria.
+        /// </summary>
+        /// <param name="accountNumber">Número de la cuenta bancaria.</param>
+        /// <returns>El saldo actual de la cuenta.</returns>
+        Task<decimal> GetBalanceAsync(string accountNumber);
+
+        /// <summary>
+        /// Realiza un depósito en una cuenta bancaria.
+        /// </summary>
+        /// <param name="accountNumber">Número de la cuenta bancaria.</param>
+        /// <param name="transactionDto">Objeto que contiene los datos de la transacción de depósito.</param>
+        /// <returns>Un objeto que representa la respuesta de la transacción de depósito.</returns>
+        Task<TransactionResponseDto> DepositAsync(string accountNumber, TransactionCreateDto transactionDto);
+
+        /// <summary>
+        /// Realiza un retiro de una cuenta bancaria.
+        /// </summary>
+        /// <param name="accountNumber">Número de la cuenta bancaria.</param>
+        /// <param name="transactionDto">Objeto que contiene los datos de la transacción de retiro.</param>
+        /// <returns>Un objeto que representa la respuesta de la transacción de retiro.</returns>
+        Task<TransactionResponseDto> WithdrawAsync(string accountNumber, TransactionCreateDto transactionDto);
+
+        /// <summary>
+        /// Obtiene las transacciones realizadas en una cuenta bancaria.
+        /// </summary>
+        /// <param name="accountNumber">Número de la cuenta bancaria.</param>
+        /// <returns>Una colección de objetos que representan las transacciones de la cuenta.</returns>
+        Task<IEnumerable<TransactionResponseDto>> GetTransactionsAsync(string accountNumber);
+    }
+}
---
+++ PruebaTecnicaBank.Infrastructure/Mappings/MappingProfile.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using PruebaTecnicaBank.Core.DTOs;
+using PruebaTecnicaBank.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PruebaTecnicaBank.Infrastructure.Mappings
+{
+    // <summary>
+    // Clase que define los perfiles de mapeo para AutoMapper.
+    // </summary>
+    public class MappingProfile : Profile
+    {
+        // <summary>
+        // Constructor que configura los mapeos entre DTOs y entidades.
+        // </summary>
+        public MappingProfile()
+        {
+            // Mapeo para Cliente
+            CreateMap<ClientCreateDto, Client>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore()); // Ignorar el Id al crear un cliente
+
+            CreateMap<Client, ClientResponseDto>(); // Mapeo de Cliente a ClienteResponseDto
+
+            // Mapeo para Cuenta
+            CreateMap<AccountCreateDto, Account>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore()) // Ignorar el Id al crear una cuenta
+                .ForMember(dest => dest.AccountNumber, opt => opt.Ignore()) // Ignorar el número de cuenta
+                .ForMember(dest => dest.Balance, opt => opt.MapFrom(src => src.InitialBalance)); // Asignar el saldo inicial
+
+            CreateMap<Account, AccountResponseDto>(); // Mapeo de Cuenta a AccountResponseDto
+
+            // Mapeo para Transacción
+            CreateMap<Transaction, TransactionResponseDto>()
+                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.ToString())); // Convertir el tipo de transacción a string
+        }
+    }
+}
